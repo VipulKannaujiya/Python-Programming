c5296842daf2d74e4ae743d2e3701183dc023381
Vipul Kannaujiya <vipulkannaujiya958@gmail.com>
2026-06-12 16:03:36 +0530

    Pyramid Pattern

---
+++ Pyramid Pattern.py
@@ -0,0 +1,12 @@
+i = 1
+while(i <= 5):
+    j = 1
+    while(j <= 5-i):
+        print(" ", end="")
+        j += 1
+    k = 1
+    while(k <= i):
+        print(" *", end="") # Star ke pahle ak space add karne se ye pyramid pattern ban jayega.
+        k += 1
+    print()
+    i += 1  
