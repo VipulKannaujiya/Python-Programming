3df69f6b26ea4fef5d79d340f4065d001debe998
Vipul Kannaujiya <vipulkannaujiya958@gmail.com>
2026-07-16 18:32:26 +0530

    Introduction To Dictionary

---
+++ Introduction to Dictionary.py
@@ -0,0 +1,11 @@
+# A dictionary is a cllection which is unordered, changeable and indexed. In Python dictionariesa are written within curly brackets, and they have kaeys and values. Means the dictionary contains two things first is the key and the second is the value.
+
+# create and print dictionary
+
+dict1 = {
+    "Brand" : "BMW",
+    "Model" : "M3",
+    "Price" : 8995500.98,
+    "Year" : 2027
+}
+print(dict1)
